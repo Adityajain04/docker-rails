16b1167f9d1e94be640bed6caedb2a83c0c368fa
Aditya Jain <adityajain.mit@gmail.com>
2020-03-25 18:44:52 +0530

    Add user list for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  scope :normal, -> { includes(:roles).references('roles').where('roles.name = ?', 'normal') }
+  scope :admin, -> { includes(:roles).references('roles').where('roles.name = ?', 'admin') }
+
+  def full_name
+    if first_name.present? and middle_name.present? and  last_name.present?
+      "#{first_name} #{middle_name} #{last_name}"
+    elsif first_name.present? and last_name.present?
+      "#{first_name} #{last_name}"
+    else
+      "#{first_name}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,  :controllers => { :registrations => "users/registrations", :sessions => "users/sessions" }
   root 'home#index'
+
+  namespace :admin do
+    get '/normal_users', to: 'users#normal_users'
+    get '/admin_users', to: 'users#admin_users'
+  end
 end
