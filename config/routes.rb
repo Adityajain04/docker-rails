479d7a433c6808498cfebbcad974d2353575656a
Aditya Jain <adityajain.mit@gmail.com>
2020-03-26 21:08:40 +0530

    Allow admin to add multiple roles to single user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get '/normal_users', to: 'users#normal_users'
     get '/admin_users', to: 'users#admin_users'
+    resources :users, only: [:edit, :update]
     resources :roles, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end
