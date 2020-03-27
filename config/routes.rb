b4248706f58117cb2b7cadbada0bc66177b9c343
Aditya Jain <adityajain.mit@gmail.com>
2020-03-27 15:11:05 +0530

    Add product controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     get '/normal_users', to: 'users#normal_users'
     get '/admin_users', to: 'users#admin_users'
     resources :users, only: [:edit, :update]
-    resources :roles, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :roles, :products, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end
