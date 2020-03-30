18f569a4c21cecde7284d2977070dd4f82cf83bf
Aditya Jain <adityajain.mit@gmail.com>
2020-03-30 15:51:08 +0530

    Add js dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+import 'bootstrap';
+import './stylesheets/application.scss';
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
+global.toastr = require("toastr")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
