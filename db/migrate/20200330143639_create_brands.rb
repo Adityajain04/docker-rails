d74d35014321b76a1bd3a655ef86d5b4fdd08799
Aditya Jain <adityajain.mit@gmail.com>
2020-03-30 21:10:33 +0530

    Add  brand table

---
+++ db/migrate/20200330143639_create_brands.rb
@@ -0,0 +1,13 @@
+class CreateBrands < ActiveRecord::Migration[6.0]
+  def change
+    create_table :brands do |t|
+      t.string :name
+      t.string :code
+      t.boolean :status, default: false
+      t.text :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_125304) do
+ActiveRecord::Schema.define(version: 2020_03_30_143639) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.boolean "status", default: false
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_brands_on_user_id"
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -29,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_03_30_125304) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -77,4 +90,5 @@ ActiveRecord::Schema.define(version: 2020_03_30_125304) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "brands", "users"
 end
