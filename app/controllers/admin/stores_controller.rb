e64f859bf5a02199f12b20393d70ea2f0132bc7c
Aditya Jain <adityajain.mit@gmail.com>
2020-03-31 18:13:58 +0530

    Add stores controller and routes

---
+++ app/controllers/admin/stores_controller.rb
@@ -0,0 +1,59 @@
+class Admin::StoresController < Admin::AdminController
+  before_action :set_store, only: [:edit, :update, :destroy]
+
+  def index
+    @stores = Store.all
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+    @store.user = current_user
+    if @store.save
+      flash[:notice] = 'Product Store created successfully.'
+      redirect_to admin_stores_path
+    else
+      flash[:alert] = @store.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @store.update(store_params)
+      flash[:notice] = 'Product Store updated successfully.'
+      redirect_to admin_stores_path
+    else
+      flash[:alert] = @store.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  def destroy
+    if @store.destroy
+      flash[:notice] = 'Product Store removed successfully.'
+      redirect_to admin_stores_path
+    else
+      flash[:alert] = 'Something went wrong.'
+      redirect_to admin_stores_path
+    end
+  end
+
+  private
+
+  def store_params
+    params.require(:brand).permit(:name, :code, :description, :user_id)
+  end
+
+  def set_store
+    @store = Store.find_by(id: params[:id])
+    unless @store.present?
+      flash[:alert] = "Product Store not found."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     get '/normal_users', to: 'users#normal_users'
     get '/admin_users', to: 'users#admin_users'
     resources :users, only: [:edit, :update]
-    resources :roles, :products, :categories, :brands, only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :roles, :products, :categories, :brands, :stores, only: [:index, :new, :create, :edit, :update, :destroy]
   end
 end
