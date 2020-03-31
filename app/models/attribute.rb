44da8d7c0ce09dd3fb3eeb1a2a4908ebf90bf501
Aditya Jain <adityajain.mit@gmail.com>
2020-03-31 18:46:03 +0530

    Add association

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,3 +1,5 @@
 class Attribute < ApplicationRecord
   belongs_to :user
+
+  has_many :attribute_values, dependent: :destroy
 end
--- app/models/attribute_value.rb
+++ app/models/attribute_value.rb
@@ -1,3 +1,4 @@
 class AttributeValue < ApplicationRecord
   belongs_to :user
+  belongs_to :attribute
 end
