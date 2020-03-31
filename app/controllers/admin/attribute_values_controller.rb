1f7b7ba17c375fa75dc8f719f400232aeb0fbbb3
Aditya Jain <adityajain.mit@gmail.com>
2020-03-31 19:21:44 +0530

    Add attribute value controller

---
+++ app/controllers/admin/attribute_values_controller.rb
@@ -0,0 +1,68 @@
+class Admin::AttributeValuesController < Admin::AdminController
+  before_action :set_attribute
+  before_action :set_attribute_value, only: [:edit, :update, :destroy]
+
+  def index
+    @attribute_values = @attribute.attribute_values
+  end
+
+  def new
+    @attribute_value = @attribute.attribute_values.new
+  end
+
+  def create
+    @attribute_value = @attribute.attribute_values.new(attribute_value_params)
+    @attribute_value.user = current_user
+    if @attribute_value.save
+      flash[:notice] = 'Attribute Value created successfully.'
+      redirect_to admin_attribute_attribute_values_path(@attribute)
+    else
+      flash[:alert] = @attribute_value.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @attribute_value.update(attribute_value_params)
+      flash[:notice] = 'Attribute Value updated successfully.'
+      redirect_to admin_attribute_attribute_values_path(@attribute)
+    else
+      flash[:alert] = @attribute_value.errors.full_messages.join(', ')
+      render :edit
+    end
+  end
+
+  def destroy
+    if @attribute_value.destroy
+      flash[:notice] = 'Attribute Value removed successfully.'
+      redirect_to admin_attribute_attribute_values_path(@attribute)
+    else
+      flash[:alert] = 'Something went wrong.'
+      redirect_to admin_attribute_attribute_values_path(@attribute)
+    end
+  end
+
+  private
+
+  def attribute_value_params
+    params.require(:attribute_value).permit(:name, :code, :description, :user_id, :attribute_id)
+  end
+
+  def set_attribute
+    @attribute = Attribute.find_by(id: params[:attribute_id])
+    unless @attribute.present?
+      flash[:alert] = "Product Attribute not found."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def set_attribute_value
+    @attribute_value = AttributeValue.find_by(id: params[:id])
+    unless @attribute_value.present?
+      flash[:alert] = "AttributeValue not found."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+end
